406ecab5f68730600aaf988ff49ef85992dfbc6b
Ayoub <ayoublaayar390@gmail.com>
2025-11-21 15:54:10 +0000

    Setup Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/User-Managment/", // Exact repo name
+  base: "/My-React-Vite-App/", // Exact repo name
 })
